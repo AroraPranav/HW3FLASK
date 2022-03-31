483a9c109fb32dc3ed1b594ee064bc85a43dfedd
Pranav Arora <89612594+AroraPranav@users.noreply.github.com>
2022-03-31 11:59:11 -0700

    Add files via upload

--- home.py
+++ home.py
@@ -1,13 +1,12 @@
 from flask import Flask
 myobj = Flask(__name__)
 
-
 name = 'Lisa'
 city_names = ['Paris', 'London', 'Rome', 'Tahiti']
 
-
 @myobj.route("/")
 def home():
+    city=""
     for i in city_names:
         city += f'<li>{i}</li>'
 
