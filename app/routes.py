330c629ac04e350d11fe17e96213b30542056b65
Pranav Arora <89612594+AroraPranav@users.noreply.github.com>
2022-03-31 11:46:52 -0700

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ city_names = ['Paris', 'London', 'Rome', 'Tahiti']
 
 @myobj.route("/", methods =["GET", "POST"])
 def home():
-    # if request.method == "POST":
-    #     name = request.form.get("cname")
-    #     return f'''<li>{name}</li>''' 
-    return render_template('home.html', title='home', len = len(city_names), name = name, city_names=city_names, added = request.form.get("cname"))
+    return render_template('home.html', title='home', 
+    len = len(city_names), 
+    name = name, city_names=city_names, 
+    added = request.form.get("cname"))
